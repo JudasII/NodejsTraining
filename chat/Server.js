a06b5444283272fe57542a3979443c9e79868b50
LAPTOP-HCATFVOS\jua-d <juan.david.208080@gmail.com>
2021-03-15 16:42:08 -0500

    adding a get messages function

--- chat/Server.js
+++ chat/Server.js
@@ -4,6 +4,19 @@ var app = express()
 
 app.use(express.static(__dirname))
 
+var messages = [
+    {
+        name:'David',
+        message:'hi'
+    },
+    {
+        name:'Juan',
+        message:'hello'
+    }
+]
+app.get('/messages', (req,res)=>{
+    res.send(messages)
+})
 var server = app.listen(3000, ()=>
     console.log('server is listening to port ', server.address().port)
 )
