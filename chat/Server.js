c88788af92fad5b1c22ed56d606e9f1a35bc70ca
LAPTOP-HCATFVOS\jua-d <juan.david.208080@gmail.com>
2021-03-15 19:03:10 -0500

    adding post messages function

--- chat/Server.js
+++ chat/Server.js
@@ -1,8 +1,10 @@
 var express = require ('express')
-
+var bodyParser = require('body-parser')
 var app = express()
 
 app.use(express.static(__dirname))
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended:false}))
 
 var messages = [
     {
@@ -12,10 +14,15 @@ var messages = [
     {
         name:'Juan',
         message:'hello'
-    }
-]
+    }]
+
 app.get('/messages', (req,res)=>{
     res.send(messages)
+})
+
+app.post('/messages', (req,res)=>{
+    messages.push(req.body)
+    res.sendStatus(200)
 })
 var server = app.listen(3000, ()=>
     console.log('server is listening to port ', server.address().port)
