3131ab59e8824a5bf024b72c1e1157249820f176
LAPTOP-HCATFVOS\jua-d <juan.david.208080@gmail.com>
2021-03-16 20:01:55 -0500

    adding database connections and simple model

--- chat/Server.js
+++ chat/Server.js
@@ -3,23 +3,37 @@ var bodyParser = require('body-parser')
 var app = express()
 var http = require ('http').Server(app)
 var io= require ('socket.io')(http)
+var mongoose = require ('mongoose')
+
+// for the porpuse of learning this credentials will remain here, however in real production enviroments they will be in a save place.
+var conString = 'mongodb+srv://dbuser:1234@chatdb.cz7lb.mongodb.net/chatdb?retryWrites=true&w=majority'
 
 app.use(express.static(__dirname))
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended:false}))
 
-var messages = []
+var Message = mongoose.model('Message',{
+    name: String,
+    message: String
+})
 
 app.get('/messages', (req,res)=>{
-    res.send(messages)
+    Message.find({},(err,messages)=>
+        res.send(messages)
+    )
 })
 
 app.post('/messages', (req,res)=>{
-    messages.push(req.body)
-    io.emit('message',req.body)
-    res.sendStatus(200)
-})
+    var message = new Message(req.body)
+    message.save(err =>{
+        if(err)
+            sendStatus(500)
+        io.emit('message',req.body)
+        res.sendStatus(200)
+    })
 
+})
+mongoose.connect(conString, {useNewUrlParser:true, useUnifiedTopology:true }, err => console.log('mongodb connection: ', err))
 var server = http.listen(3000, ()=>
     console.log('server is listening to port ', server.address().port)
 )
